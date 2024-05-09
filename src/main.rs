3619ff5d65566d5b235e4155f4c635a6ed7a8d43
Lucas Aponso <s3896348@student.rmit.edu.au>
2024-05-09 14:38:49 +0000

    Added example code for variables in rust.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,53 @@
-fn main() //using the & operator. This is better becuase we can reference the array rather than send the whole array into the function
+fn main()
 {
-    let arry: [i32; 5] = [1,2,2,4,3]; //must populate array with values, declare the size and the data type.
-    let sum = add_numbers(&arry); //add_numbers returns the sum of all the numbers
-    println!("The sum of all the numbers is {}", sum);
+    println!("HelloWorld");
 }
 
+// fn main()
+// {
+//     let x = 4;
+//     println!("X is {}", x);
+// }
 
-fn add_numbers(arr: &[i32]) -> i32 //this declares the function needs a reference to an array. And it should return a int
-{
-    let mut total: i32 = 0; //initialise total
-    for &num in arr
-    {
-        total = total + num;
-    }
-    total
-}
+
+// fn main()
+// {
+//     let x = 4;
+//     x = 3;
+//     println!("X is {}", x);
+// }
+
+// fn main()
+// {
+//     let mut x = 4;
+//     x 2;
+//     println!("X is {}", x);
+// }
+
+// fn main()
+// {
+//     let x = 4;
+//     let x = 3;
+//     println!("X is {}", x);
+// }
+
+// fn main()
+// {
+//     let x:i32 = 4;
+//     println!("X is {}", x);
+// }
+
+// fn main()
+// {
+//     let x:u8 = 4;
+//     println!("X is {}", x);
+// }
+
+// fn main()
+// {
+//     let x:u16 = 4;
+//     println!("X is {}", x);
+// }
+
+
+
